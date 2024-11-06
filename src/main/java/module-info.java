4105b1ac67a751674aa2932e42139a7a36c9a25d
TronchamulasF <mroskos2909@g.educaand.es>
2024-11-06 10:32:04 +0100

    Version 0.5

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,13 @@ module com.example.conexionbbdd {
     requires javafx.controls;
     requires javafx.fxml;
 
+    requires java.sql;
+    requires org.postgresql.jdbc;
+
+    opens model to javafx.base;
+    opens view to javafx.fxml;
+
+
 
     opens com.example.conexionbbdd to javafx.fxml;
     exports com.example.conexionbbdd;
@@ -9,4 +16,6 @@ module com.example.conexionbbdd {
     opens com.example.conexionbbdd to javafx.fxml;
     exports com.example.conexionbbdd.Controladores;
     opens com.example.conexionbbdd.Controladores to javafx.fxml;
+    exports com.example.conexionbbdd;
+    exports view;
 }
